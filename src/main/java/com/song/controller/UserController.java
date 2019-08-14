caea5b98a827db1f5145f5efa647d8b10e557158
songbaolin <784574192@qq.com>
2019-08-14 10:31:23 +0800

    增加二次main方法

--- src/main/java/com/song/controller/UserController.java
+++ src/main/java/com/song/controller/UserController.java
@@ -3,5 +3,6 @@ package com.song.controller;
 public class UserController {
 public static void main(String[] args) {
 	System.out.println("ssssssssssss");
+	System.out.println("ssssssssssssssssssssssss");
 }
 }
